6a19f8fad096a00a550b77682d43edb8414b911f
EkOzhegova <ozhegova.ek@gmail.com>
2020-10-21 00:25:41 +0500

    2nd task(again) D:

---
+++ DigitsReverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DigitsReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 4х-значное число.");
+            string s = Console.ReadLine();
+            char[] str = s.ToCharArray();
+            string a;
+            for (int i = str.Length - 1; i > -1; --i)
+            {
+                a = Convert.ToString(str[i]);
+                Console.Write(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}

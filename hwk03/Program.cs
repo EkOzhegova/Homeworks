515d72333c2b713aa82dcf4bc0f3a240524387e1
EkOzhegova <ozhegova.ek@gmail.com>
2020-10-21 00:07:04 +0500

    3rd task

---
+++ hwk03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework03
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение x");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = F(x);
+
+            Console.WriteLine("f(x) = " + y);
+            Console.ReadKey();
+        }
+
+        static double F(double x)
+        {
+            return 1 + Math.Sqrt((x * x - 1) / (x * x + 1));
+        }
+    }
+}

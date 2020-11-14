07a619918eea4120c398889729e482cb16ae44d3
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2020-11-14 23:18:26 +0300

    hw2
    вариант 10

---
+++ HomeWork02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork02
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите четырехзначное число: ");
+            var number = int.Parse(Console.ReadLine());
+
+            int a, b, c, d, result;
+            a = number % 10;
+            b = (number % 100 - number % 10) / 10;
+            c = (number % 1000 - number % 100) / 100;
+            d = (number / 1000);
+            result = a * 1000 + b * 100 + c * 10 + d;
+            Console.WriteLine($"Полученное число: " + result);
+            Console.ReadKey();
+
+        }
+    }
+}

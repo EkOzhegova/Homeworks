ffcbbfb812b591ad8c6f34df89475d8f913de507
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2020-11-14 15:19:34 +0300

    задание 5.2
    Ожегова Екатерина уги-106004

---
+++ hw5.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		String str = "клавиатура";
+		String tiara = str.Substring(6, 1) + str.Substring(4, 2) + str.Substring(8); 
+		String hand = str.Substring(8, 1) + str.Substring(7, 1) + str.Substring(0, 1) + str.Substring(2, 1);
+		Console.WriteLine("Исходное слово: " + str);
+		Console.WriteLine("Получившиеся слова: " + tiara + ", " + hand);
+	}
+}

69d9823bdb50ba22c9cd8c12272ca3f8d75d1073
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2020-11-24 12:46:35 +0300

    Задание 7
    вариант 10

---
+++ HW7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW7
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x, function;
+            Console.WriteLine("Введите значение X:");
+            x = double.Parse(Console.ReadLine());
+            if (x < -1)
+            {
+                function = 1;
+                Console.WriteLine("Значение функции F(x) равно" + function);
+            }
+            else
+                if (x >= -1 && x <= 1)
+            {
+                function = -x;
+                Console.WriteLine("Значение функции F(x) равно" + function);
+            }
+            else
+                if (x > 1)
+            {
+                function = -1;
+                Console.WriteLine("Значение функции F(x) равно" + function);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

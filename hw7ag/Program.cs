bb374c5fbb25c99e5f968ef504ba8ea19b31c4cf
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2020-12-27 16:42:37 +0500

    задание 7 переделано

---
+++ hw7ag/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW7_again
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Введите значение X:");
+           double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение функции равно" + GetValueOfFunction(x));
+
+            Console.ReadKey();
+
+        }
+        static double GetValueOfFunction ( double x)
+        {
+            if (x < -1)
+            return 1;
+
+            if (x >= -1 && x <= 1)
+                return -x;
+
+            else
+                return -1;
+           
+        }
+    }
+}

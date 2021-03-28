f78879dc8ed6db731a8668c1281cf92608435884
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2021-03-28 18:52:02 +0500

    Библиотека
    вариант 10

---
+++ BookLibrary/BookLibraryLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BookLibraryLibrary
+{
+    public class Reader
+    {
+        public string Name;
+        public string Surname;
+        public int LibraryCardNumber;
+        public string TakenLiterature;
+        public DateTime DateOfIssue;
+        public TimeSpan AmountOfDays;
+        public DateTime ReturnDate;
+        public double PiedgeAmount;
+
+        public Reader(string name, string surname, int cardNumber)
+        {
+            Name = name;
+            Surname = surname;
+            LibraryCardNumber = cardNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            Console.WriteLine($"Номер читательского билета: {LibraryCardNumber}. Список названий взятой литературы: {TakenLiterature}. Дата выдачи: {DateOfIssue.ToShortDateString()}. Срок выдачи: {AmountOfDays.ToString("%d")}. Дата планируемого возвращения: {ReturnDate.ToShortDateString()}. Сумма залога: {PiedgeAmount}.");
+        }
+    }
+}
---
+++ BookLibrary/LibraryUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LibraryUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LibraryUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f25e126-a4b7-4a23-934f-81621504ca24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

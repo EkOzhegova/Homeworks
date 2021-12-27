d971ea96c81122bb2902428b40dc3ee6aa5becf4
EkOzhegova <72496589+EkOzhegova@users.noreply.github.com>
2021-12-27 23:42:01 +0500

    21hw6

---
+++ 6/PhotoEnhancer/PhotoEnhancer/Filters/Parameters/GammaCorrectionParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    class GammaCorrectionParameters : IParameters
+    {
+        [ParameterInfo(Name = "Коэффициент", MinValue = 0.2, MaxValue = 5, DefaultValue = 1, Increment = 0.01)]
+        public double Coefficent { get; set; }
+    }
+}
